572d0a4cc3f420042ebc86700090fae57675d5bb
sahoochinmay <sahoochinmay54@gmail.com>
2022-06-30 18:32:42 +0530

    Login and signup page modificaiotn continue

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,18 +2,18 @@
     <i class="fa fa-bars" aria-hidden="true" id="menuBtn"></i>
     <div class="rowDivide">
         <div class="logo">
-            <a href="index.html"><img src="assets/images/logomain.png" alt=""></a>
+            <a href="index.php"><img src="assets/images/logomain.png" alt=""></a>
         </div>
         <div class="links">
             <ul>
-                <li><a href="privacypolicy.html">Privacy Policy</a></li>
-                <li><a href="about.html">About us</a></li>
-                <li><a href="contact.html">Support</a></li>
+                <li><a href="privacypolicy.php">Privacy Policy</a></li>
+                <li><a href="about.php">About us</a></li>
+                <li><a href="contact.php">Support</a></li>
             </ul>
         </div>
         <div class="loginSignup">
-            <a href="signup.html" id="primaryBtn">Sign in</a>
-            <a href="signup.html" id="secondaryBtn">create a free account</a>
+            <a href="login.php" id="primaryBtn">Sign in</a>
+            <a href="login.php" id="secondaryBtn">create a free account</a>
         </div>
     </div>
 </nav>
@@ -21,10 +21,10 @@
 
 <section class="sideNav" id="sideNav">
     <ul>
-        <li><a href="privacypolicy.html">Privacy Policy</a></li>
-        <li><a href="about.html">About Us</a></li>
-        <li><a href="contact.html">Support</a></li>
-        <li><a href="signup.html">Login</a></li>
-        <li><a href="signup.html">Create a free account</a></li>
+        <li><a href="privacypolicy.php">Privacy Policy</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="contact.php">Support</a></li>
+        <li><a href="signup.php">Login</a></li>
+        <li><a href="signup.php">Create a free account</a></li>
     </ul>
 </section>
