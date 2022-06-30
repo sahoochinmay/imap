2bccbbdb993700170b1f0086e3093874891964fa
sahoochinmay <sahoochinmay54@gmail.com>
2022-06-30 18:24:08 +0530

    Index page all modification completed

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+  <!---Footer Section-->
+  <section class="footer">
+      <div class="rowDivide">
+          <div class="logo">
+              <a href="#"> ©2022 iMail.&nbsp; All rights reserved</a>
+          </div>
+          <div class="links">
+              <ul>
+                  <li><a href="privacypolicy.html">Privacy Policy</a></li>
+                  <li><a href="about.html">About us</a></li>
+                  <li><a href="contact.html">Support</a></li>
+              </ul>
+          </div>
+      </div>
+  </section>
---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+<nav>
+    <i class="fa fa-bars" aria-hidden="true" id="menuBtn"></i>
+    <div class="rowDivide">
+        <div class="logo">
+            <a href="index.html"><img src="assets/images/logomain.png" alt=""></a>
+        </div>
+        <div class="links">
+            <ul>
+                <li><a href="privacypolicy.html">Privacy Policy</a></li>
+                <li><a href="about.html">About us</a></li>
+                <li><a href="contact.html">Support</a></li>
+            </ul>
+        </div>
+        <div class="loginSignup">
+            <a href="signup.html" id="primaryBtn">Sign in</a>
+            <a href="signup.html" id="secondaryBtn">create a free account</a>
+        </div>
+    </div>
+</nav>
+
+
+<section class="sideNav" id="sideNav">
+    <ul>
+        <li><a href="privacypolicy.html">Privacy Policy</a></li>
+        <li><a href="about.html">About Us</a></li>
+        <li><a href="contact.html">Support</a></li>
+        <li><a href="signup.html">Login</a></li>
+        <li><a href="signup.html">Create a free account</a></li>
+    </ul>
+</section>
