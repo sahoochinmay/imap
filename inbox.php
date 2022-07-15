2fd406ba9f74b52cd908ed5c1dd2008369c5cdc3
sahoochinmay <sahoochinmay54@gmail.com>
2022-07-16 01:22:52 +0530

    Show mail details completed

--- inbox.php
+++ inbox.php
@@ -16,6 +16,9 @@ $user = $_SESSION['email'];
 </head>
 
 <body>
+    <script>
+
+    </script>
     <!-- Inside Nav -->
     <?php include "./includes/insideNavbar.php" ?>
     <main class="mailbox_main">
@@ -36,12 +39,12 @@ $user = $_SESSION['email'];
                         <?php
                         $sql = "Select * from mail where MailBox='inbox' and owner='$user' order by dttime desc";
                         $res = mysqli_query($conn, $sql);
-
                         while ($row = mysqli_fetch_assoc($res)) {
                             $formatedDateArray =  preg_split("/\-/", $row['dttime']);
-                            // echo "<script>console.log('" . json_encode($formatedDateArray) . "');</script>";
                             $formatedDate = "$formatedDateArray[2]-$formatedDateArray[1]-$formatedDateArray[0]";
-                            echo "<tr style='border-top:5px solid rgb(247, 245, 255);'>
+                            $json_encodeValue = json_encode($row);
+                            // echo "<script>mail_data = $json_encodeValue; console.log('" . json_encode($row) . "');</script>";
+                            echo "<tr  onclick='redirectToMailBox($json_encodeValue);'  style='border-top:5px solid rgb(247, 245, 255);' >
                                     <td> <i class='fa fa-star-o' aria-hidden='true'></i> </td>
                                     <td style='max-width: calc(100vw - 700px);  white-space: nowrap;
                                     overflow: hidden;text-overflow: ellipsis;' ><b>$row[sub]&nbsp;&nbsp;-&nbsp;&nbsp;</b>$row[message]</td>
@@ -55,6 +58,12 @@ $user = $_SESSION['email'];
             </div>
         </div>
     </main>
+    <script>
+        function redirectToMailBox(value) {
+            localStorage.setItem('mail_data', JSON.stringify(value));
+            location.href = 'mailbox.php'
+        }
+    </script>
     <!-- insideBottom links -->
     <?php include "./includes/insideBottomLinks.php" ?>
 </body>
--- mailbox.php
+++ mailbox.php
@@ -10,236 +10,24 @@ $user = $_SESSION['email'];
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Header links -->
+    <?php include "./includes/headerlinks.php" ?>
     <title>Mailbox</title>
-
-    <!----BOOTSTRAp 5----->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!--------CUSTOM CSS------->
-    <link rel="stylesheet" href="css/style.css">
-
-
-    <!-- fontawsome link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <!-- tsParticle cdn link -->
-
-
-    <!----Google Font----->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-
-
-    <nav class="mailBoxNav d-flex justify-content-between  align-items-center" style="padding: 10px 40px;position: relative;">
-        <i class="fa fa-bars" aria-hidden="true" id="menuButton"></i>
-        <div class="logo">
-            <a href="mailbox.html"><img src="assets/images/logomain.png" alt="" style="width: 80px;"></a>
-        </div>
-        <div>
-            <div class="d-flex  justify-content-end  align-items-center flex-row">
-                <h5 style="margin-right: 10px;margin-top: 5px;">Welcome, Chinmay</h5>
-                <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" alt="Avatar" style="height: 35px;" />
-            </div>
-        </div>
-    </nav>
+    <!-- Inside Nav -->
+    <?php include "./includes/insideNavbar.php" ?>
     <main class="mailbox_main">
-        <div class="sidebar" id="sidebar">
-            <button> <i class="fa fa-plus" aria-hidden="true"></i> New Message</button>
-
-            <ul>
-                <li><a href="#"> <i class="fa fa-envelope" aria-hidden="true"></i> Inbox</a></li>
-                <li><a href="#"> <i class="fa fa-envelope" aria-hidden="true"></i> Drafts</a></li>
-                <li><a href="#"> <i class="fa fa-inbox" aria-hidden="true"></i> Outbox</a></li>
-                <li><a href="#"> <i class="fa fa-paper-plane" aria-hidden="true"></i> Sent</a></li>
-                <li><a href="#"> <i class="fa fa-trash" aria-hidden="true"></i> Trash</a></li>
-                <li><a href="#"> <i class="fa fa-archive" aria-hidden="true"></i> Archieve</a></li>
-            </ul>
-        </div>
+        <!-- Inside Nav -->
+        <?php include "./includes/sidebar.php" ?>
         <div class="mailArea">
-
-            <!-- <div class="mailContainer">
-
-                <table class="customTable">
-                    <thead>
-                        <tr>
-                            <th style="width: 50px;"></th>
-                            <th style="width: 900px;">Message</th>
-                            <th>From</th>
-                            <th>Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                        <tr>
-                            <td> <i class="fa fa-star-o" aria-hidden="true"></i> </td>
-                            <td>You have received a message</td>
-                            <td>Chinmay</td>
-                            <td>27 jun, 8 pm</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div> -->
-
-
             <div class="mailDetailsWrapper">
-
                 <div class="mailDetails">
                     <div class="topDetails">
-
-
                         <div class="mailLinks">
                             <ul>
-
-                                <li><a href="#" class="backBtn"> <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                                <li><a href="inbox.php" class="backBtn"> <i class="fa fa-chevron-left" aria-hidden="true"></i>
                                     </a></li>
                                 <li><a href="#"> <i class="fa fa-reply-all" aria-hidden="true"></i> Reply</a></li>
                                 <li><a href="#">Forward <i class="fa fa-share" aria-hidden="true"></i> </a></li>
@@ -247,90 +35,42 @@ $user = $_SESSION['email'];
                                 <li><a href="#"> <i class="fa fa-trash" aria-hidden="true"></i> Delete </a></li>
                             </ul>
                         </div>
-
                         <div class="mailShortDetails">
-
                             <div class="shortInfo">
-                                <p>Subject </p>: <span>Project Details</span><br>
-                                <p>From</p>: <span>Chinmay</span><br>
-                                <p>To</p>: <span>Smruti</span><br>
-                                <p>Date</p>: <span>27/06/2022</span>
+                                <p>Subject </p>: <span id="subject"></span><br>
+                                <p>From</p>: <span id="from"></span><br>
+                                <p>To</p>: <span id="to"></span><br>
+                                <p>Date</p>: <span id="date"></span>
                             </div>
-
-
                             <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="" srcset="">
-
                         </div>
-
-
-
-
-
                     </div>
-
                     <div class="messageDetails">
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa praesentium vel quam numquam
-                            tempora
-                            commodi provident perspiciatis! Inventore, adipisci officia? Doloribus, consequatur. Ratione
-                            odio
-                            ullam rerum perferendis esse, saepe delectus perspiciatis asperiores praesentium eaque iste
-                            a
-                            nihil
-                            totam cerum fugit quae, ratione
-                            nesciunt error sint, aperiam ab ea quibusdam maxime deleniti exercitationem, eligendi
-                            recusandae? Harum laboriosam architecto provident corporis placeat assumenda laudantium
-                            quidem
-                            saepe, deleniti sed beatae eius <br><br> illo similique. Repudiandae nostrum magnam eveniet
-                            labore odio,
-                            fuga, eligendi ducimus eos, repellendus ut adipisci. Aliquid cupiditate alias dolores ullam
-                            ab
-                            placeat quoam aliquid vero ipsa
-                            commodi odit corporis reiciendis provident deleniti neque repudiandae numquam molestiae
-                            recusandae dolorem, distinctio rem officiis esse corrupti eos. Ipsa ad, laborum accusantium
-                            aperiam modi, ipsum officiis pariatur ex at officia nihil blanditiis quis. Nostrum tempore
-                            inventore quas earum corporis aperiam architecto doloremque qui officiis debitis! Unde earum
-                            omnis nulla aliquam? Mollitia, saepe, eligendi voluptatem sunt officia cum nobis excepturi
-                            dolorem voluptate optio perspiciatis quibusdam repellendus exercitationem. Assumenda!
+                        <p id="message" >
                         </p>
-
-                        <small>Regards, Smruti</small>
-
+                        <small id="footer"></small>
                     </div>
                 </div>
-
             </div>
-
-
-            <script>
-                var getmenuBtn = document.getElementById('menuButton');
-                var getsidebar = document.getElementById('sidebar');
-
-                // getsidebar.style.left = "-250px";
-
-
-                getmenuBtn.onclick = function() {
-                    if (getsidebar.style.left == "-250px") {
-                        getsidebar.style.left = "0px";
-
-                    } else {
-                        getsidebar.style.left = "-250px";
-
-                    }
-                }
-            </script>
-
-
-
-
-
         </div>
     </main>
-    <!-- <script src="navbar.js"></script> -->
-    <script src="js/navbarSticky.js"></script>
-    <!-------BOotstrap 5 JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <script>
+        window.onload = function() {
+            let mail_data = JSON.parse(localStorage.getItem('mail_data'))
+            console.log(mail_data)
+            if (mail_data) {
+                document.getElementById('subject').innerText = mail_data?.sub
+                document.getElementById('from').innerText = mail_data?.sender
+                document.getElementById('to').innerText = mail_data?.receiver
+                let dttimeArr = mail_data?.dttime?.split('-');
+                console.log(dttimeArr)
+                document.getElementById('date').innerText = dttimeArr[2] + '-' + dttimeArr[1] + '-' + dttimeArr[0]
+                document.getElementById('message').innerText = mail_data?.message
+            } else location.href = 'inbox.php'
+        }
+    </script>
+    <!-- insideBottom links -->
+    <?php include "./includes/insideBottomLinks.php" ?>
 </body>
 
 </html>
