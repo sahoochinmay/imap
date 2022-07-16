9e2b86209de3a48e3db75786701e399f13d3bf30
sahoochinmay <sahoochinmay54@gmail.com>
2022-07-16 15:33:43 +0530

    Sent mail integration completed

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,27 +4,32 @@
         <li id="inbox"><a id="inbox_a" href="inbox.php"> <i class="fa fa-envelope" aria-hidden="true"></i> Inbox</a></li>
         <li id="draft"><a id="draft_a" href="drafts.php"> <i class="fa fa-envelope" aria-hidden="true"></i> Drafts</a></li>
         <li><a href="#"> <i class="fa fa-inbox" aria-hidden="true"></i> Outbox</a></li>
-        <li><a href="#"> <i class="fa fa-paper-plane" aria-hidden="true"></i> Sent</a></li>
+        <li id="sent"><a id="sent_a" href="sentmails.php"> <i class="fa fa-paper-plane" aria-hidden="true"></i> Sent</a></li>
         <li><a href="#"> <i class="fa fa-trash" aria-hidden="true"></i> Trash</a></li>
         <li><a href="#"> <i class="fa fa-archive" aria-hidden="true"></i> Archieve</a></li>
     </ul>
+    <div class="logout">
+        <button onclick="window.location.href='logout.php'" id="primaryBtn">
+            <i class="fa fa-sign-out" aria-hidden="true"></i>Logout</button>
+    </div>
 </div>
 <!-- The Modal -->
 <div id="myModal" class="modal">
     <!-- Modal content -->
     <div class="modal-content">
-        <div style="display: flex; justify-content:space-between;">
-            <h5>Send Mail</h5>
+        <div class="SendMailTop" style="display: flex; justify-content:space-between; align-items:center">
+            <h5 style="margin-bottom:0">Send Mail</h5>
             <span class="close">&times;</span>
         </div>
         <hr>
         <form action="./sendmail.php" method="post" enctype="multipart/form-data">
             <div>
-                <label>To</label>
+                <label>To</label><br>
                 <input name="to" id="to" required="" class="w3-input w3-border w3-hover-shadow w3-margin-bottom" type="email" value="">
             </div>
+
             <div>
-                <label>Subject</label>
+                <label>Subject</label><br>
                 <input name="sub" id="sub" required="" class="w3-input w3-border w3-hover-shadow  w3-margin-bottom" type="text" value="">
             </div>
             <div>
@@ -33,12 +38,17 @@
             <input type="hidden" value="" name="h1" id="h1">
             <div class="w3-section">
                 <!-- <b>Attachment : </b> -->
-                <input type="hidden" value="" name="attach" id="attach">
+                <!-- <input type="hidden" value="" name="attach" id="attach"> -->
                 <!-- <input type="file" name="attachment"> -->
-                <br>
+                <!-- <br> -->
                 <a class="w3-btn w3-red" href="javascriot:void(0);" onclick="f2();">Cancel</a>
-                <input type="submit" name="btnSend" value="Send Mail" class="w3-btn w3-right w3-dark-grey">
-                <input type="submit" name="btnSave" value="Save Mail" class="w3-btn w3-right w3-green" style="margin-right:20px;">
+
+                <div class="rightSec">
+                    <input class="saveMail" type="submit" name="btnSave" value="Save Mail" class="w3-btn w3-right w3-green">
+                    <input class="sendMail" type="submit" name="btnSend" value="Send Mail" class="w3-btn w3-right w3-dark-grey" style="background:green; color:white;">
+
+                </div>
+
             </div>
             <input type="hidden" id="target" name="target" value="<?php echo "mailbox.php"; ?>" />
         </form>
@@ -49,6 +59,10 @@
         if (window.location?.pathname?.includes('inbox.php')) {
             document.getElementById('inbox').style = "background-color:#6d4aff;";
             document.getElementById('inbox_a').style = "color:white;"
+        } else if (window.location?.pathname?.includes('sentmails.php')) {
+            document.getElementById('sent').style = "background-color:#6d4aff;";
+            document.getElementById('sent_a').style = "color:white;"
         }
+
     }
 </script>
---
+++ sentmails.php
@@ -0,0 +1,71 @@
+<?php
+include './config/config.php';
+if (!isLoggedin()) {
+    header("Location:login.php");
+    exit;
+}
+$user = $_SESSION['email'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Header links -->
+    <?php include "./includes/headerlinks.php" ?>
+    <title>Sent</title>
+</head>
+
+<body>
+    <script>
+
+    </script>
+    <!-- Inside Nav -->
+    <?php include "./includes/insideNavbar.php" ?>
+    <main class="mailbox_main">
+        <!-- Inside Nav -->
+        <?php include "./includes/sidebar.php" ?>
+        <div class="mailArea">
+            <div class="mailContainer">
+                <table class="customTable">
+                    <thead>
+                        <tr>
+                            <th style="width: 50px;"></th>
+                            <th style="max-width: calc(100vw - 700px);">Message</th>
+                            <th>From</th>
+                            <th>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "Select * from mail where MailBox='sent' and owner='$user' order by dttime desc";
+                        $res = mysqli_query($conn, $sql);
+                        while ($row = mysqli_fetch_assoc($res)) {
+                            $formatedDateArray =  preg_split("/\-/", $row['dttime']);
+                            $formatedDate = "$formatedDateArray[2]-$formatedDateArray[1]-$formatedDateArray[0]";
+                            $json_encodeValue = json_encode($row);
+                            // echo "<script>mail_data = $json_encodeValue; console.log('" . json_encode($row) . "');</script>";
+                            echo "<tr  onclick='redirectToMailBox($json_encodeValue);'  style='border-top:5px solid rgb(247, 245, 255);' >
+                                    <td> <i class='fa fa-star-o' aria-hidden='true'></i> </td>
+                                    <td style='max-width: calc(100vw - 700px);  white-space: nowrap;
+                                    overflow: hidden;text-overflow: ellipsis;' ><b>$row[sub]&nbsp;&nbsp;-&nbsp;&nbsp;</b>$row[message]</td>
+                                    <td>$row[sender]</td>
+                                    <td>$formatedDate</td>
+                                 </tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+    <script>
+        function redirectToMailBox(value) {
+            localStorage.setItem('mail_data', JSON.stringify(value));
+            location.href = 'mailbox.php'
+        }
+    </script>
+    <!-- insideBottom links -->
+    <?php include "./includes/insideBottomLinks.php" ?>
+</body>
+
+</html>
