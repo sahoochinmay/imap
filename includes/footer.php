dbc195a53368d6bd471167a3c56f553e436cb4a1
sahoochinmay <sahoochinmay54@gmail.com>
2022-07-21 20:06:31 +0530

    Team member section completed

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
               <a href="#"> ©2022 iMail.&nbsp; All rights reserved</a>
           </div>
           <div class="links">
-              <p>Made with <span><i class="fa fa-heart" aria-hidden="true"></i></span> by Team SCRAP</p>
+              <p>Made with <span><i class="fa fa-heart" aria-hidden="true"></i></span> by Team SPARC</p>
           </div>
       </div>
   </section>
---
+++ includes/teamMembers.php
@@ -0,0 +1,94 @@
+<section class="team">
+
+    <h2 style="text-align:center;">OUR TEAM</h2>
+    <div class="memberRow">
+
+        <div class="memberWrapper">
+            <div class="memberDetails">
+                <img src="assets/images/smruti.jpg" alt="" srcset="">
+
+                <h4>Smruti Sourav</h4>
+                <p>Frontend Design</p>
+
+                <div class="socials">
+
+                    <a href="https://www.instagram.com/im_smruu_07/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100004369921877" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.twitter.com/Smruti_07/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="memberWrapper">
+            <div class="memberDetails">
+                <img src="assets/images/piyush.jpg" alt="" srcset="">
+
+                <h4>Piyush Kumar</h4>
+                <p>Frontend Design</p>
+
+                <div class="socials">
+
+                    <a href="https://www.instagram.com/___.p_i_y_u_s_h___/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://www.facebook.com/piyush.dikshit.73/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.twitter.com/PiyushDikshit3/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="memberWrapper">
+            <div class="memberDetails">
+                <img src="assets/images/asutosh.jpg" alt="" srcset="">
+
+                <h4>Asutosh</h4>
+                <p>Database Manager</p>
+
+                <div class="socials">
+
+                    <a href="https://www.instagram.com/asu_dash001/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://www.facebook.com/100037376975151/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.twitter.com/asutosh_786/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="memberWrapper">
+            <div class="memberDetails">
+                <img src="assets/images/rishi.jpg" alt="" srcset="">
+
+                <h4>Rishi Pratim</h4>
+                <p>Content Creator</p>
+
+                <div class="socials">
+
+                    <a href="https://www.instagram.com/_.rishi____" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.twitter.com/pratim_rishi/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="memberWrapper">
+            <div class="memberDetails">
+                <img src="assets/images/chinmay.jpg" alt="" srcset="">
+
+                <h4>Chinmay Ranjan</h4>
+                <p>Full Stack Developer</p>
+
+                <div class="socials">
+
+                    <a href="https://www.instagram.com/_sahoochinmay/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://www.facebook.com/sahoochinmay.R/" target="_target"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.twitter.com/_sahoochinmay/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
